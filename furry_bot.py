3deb78b714f49a8315e255adb03d4f6d392c124f
Julio Cesar <jcmcs177@gmail.com>
2022-11-19 18:22:30 -0300

    Notification and reminder system complete

--- furry_bot.py
+++ furry_bot.py
@@ -1,7 +1,6 @@
 import discord
 import json
 import requests
-import re
 import os
 from discord.ext import commands, tasks
 
@@ -14,51 +13,72 @@ furry = commands.Bot(command_prefix="!", description=description, intents=intent
 @furry.event
 async def on_ready():
     print(f"Logged in as {furry.user} (ID: {furry.user.id})")
-    
-    with open("raposow_channel_data.json", "r+") as f:
-        data = json.load(f)
-
-        if not data["notyfing_channel_id"]:
-            for channel in furry.get_all_channels():
-                if (channel.type == discord.ChannelType.text or channel.type == discord.ChannelType.news) and re.search("^.*v[ií]deo(s)?.*", channel.name.lower()):
-                    data["notyfing_channel_id"] = channel.id
-                    json.dump(data, f)
-                    break
 
 @furry.command()
 async def oi(ctx):
     await ctx.send("Olá!")
 
-@furry.command()
-async def notifiqueAqui(ctx):
-    return
-
 @tasks.loop(seconds=30)
 async def checkForShorts():
-    with open("raposow_channel_data.json", "r+") as f:
+    with open("raposow_channel_data.json", "r") as f:
         saved_data = json.load(f)
 
+        # Sending GET request to API
         channel_data = requests.get(f"https://yt.lemnoslife.com/channels?part=shorts&id={saved_data['channel_id']}")
         if channel_data.status_code != 200:
             print(f"Request to API returned code {channel_data.stauts_code}")
         else:
+            #  Getting short's id in index 0 of the data sent by the API
             latest_short_id = channel_data.json()["items"][0]["shorts"][0]["videoId"]
+            print(f"Successfully sent GET code to API. Found last short id {latest_short_id}; Title: \"{channel_data.json()['items'][0]['shorts'][0]['title']}\"")
+
             if latest_short_id == saved_data["latest_short_id"]:
                 print("No shorts posted since last notification.")
             else:
                 saved_data["latest_short_id"] = latest_short_id
-                json.dump(saved_data, f)
+                
+                with open("raposow_channel_data.json", "w") as f:
+                    json.dump(saved_data, f)
 
-                discord_channel_id = saved_data["notyfing_channel_id"]
+                discord_channel_id = saved_data["notifying_channel_id"]
                 discord_channel = furry.get_channel(discord_channel_id)
 
-                msg = f"@everyone o Raposow ACABOU de postar um shorts novíssimo! Vai lá ver agora: {'https://www.youtube.com/shorts/' + latest_short_id}"
-                discord_channel.send(msg)
+                msg = f"**Raposow ACABOU de postar um shorts! Corre lá pra ver:**\n*{'https://www.youtube.com/shorts/' + latest_short_id}*\n\n||@everyone||"
+                await discord_channel.send(msg)
+
+@tasks.loop(seconds=10)
+async def suggestionReminder():
+    with open("raposow_channel_data.json", "r") as f:
+        data = json.load(f)
+
+        suggestion_channel_id = data["suggestions_channel_id"]
+        suggestion_channel = furry.get_channel(suggestion_channel_id)
+
+        await suggestion_channel.send("**@here UTILIZE O COMANDO:**   ,sugerir")
+
+
+async def setChannelForTask(channel_json_key, task_f, ctx, response):
+    with open("raposow_channel_data.json", "r") as f:
+        data = json.load(f)
+        data[channel_json_key] = ctx.channel.id
+        
+        with open("raposow_channel_data.json", "w") as f:
+            json.dump(data, f)
+
+    task_f.start()
+    await ctx.send(response)
+
+@furry.command()
+async def notifiqueAqui(ctx):
+    await setChannelForTask("notifying_channel_id", checkForShorts, ctx, f"Notificando agora no canal \"{ctx.channel.name}\"")
+
+@furry.command()
+async def sugestoesAqui(ctx):
+    await setChannelForTask("suggestions_channel_id", suggestionReminder, ctx, f"Sugestões no canal {ctx.channel.name}")
 
 
 token = os.getenv("FURRY_BOT_TOKEN")
 if not token:
-    print("Unable to get token from environment variables.")
-    exit()
+    raise RuntimeError("Unable to get token from environment variables.")
 
 furry.run(token)
